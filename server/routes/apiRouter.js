70b15186116a9d71ec6fc28b683e857e34de2675
James Boswell <jamesboswell@Jamess-MacBook-Pro.local>
2023-08-05 10:28:33 -0400

    working boilerplate

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path= require('path');
 
 /*Controllers */
-//const userController = require('../controllers/user');
+const userController = require('../controllers/userController.js');
 
 const apiRouter = express.Router();
 
 
 
-//apiRouter.get('user/:id',userController.getUser,((req,res)=>{
-
-//})
+apiRouter.get('user/:id',userController.getUser,((req,res)=>{
+    res.status(200).json({});
+})
 
 
 apiRouter.get('/',(req,res)=>{
