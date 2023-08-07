657ebb726e4c67548d958f5e91ff296c9e39af98
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2023-08-07 15:35:12 -0400

    working on contract display component

---
+++ client/components/Contract.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+const Contract = async () => {
+
+    const [contractInfo, setContractInfo] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const findContract = async () => {
+            try {
+                const foundContract = await fetch('./api/contract', {
+                    method: "GET"
+                  })
+                setContractInfo(foundContract)
+            }
+
+            catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        }
+    }, [])
+
+    if (loading) {
+        return <div>Loading...</div>;
+      }
+    
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+    
+    
+    const username = 'Oliver'
+    const date = 'august 12th';
+    const habit = contractInfo.tangibles[0].desc;
+    const frequency = contractInfo.tangibles[0].count;
+    const duration = contractInfo.tangibles[0].numberOfWeeks;
+    
+    return (
+        <div>
+            <p>I, {username} commited to {habit} {frequency} times per week for {duration} weeks.</p>
+            <p>signed</p>
+            <img></img>
+        </div>
+    )
+}
+
+
+
+export default Contract;
