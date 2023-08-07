90fa2e3683a91f70dc6e135077a7d1417bdfc91c
James Boswell <jamesboswell@Jamess-MacBook-Pro.local>
2023-08-07 10:48:58 -0400

    functional login and create complete

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,23 +1,35 @@
 import React from "react"
 import { useNavigate } from 'react-router-dom'
 import { useState } from "react"
+import axios from "axios"
 const Login = () => {
     const nav = useNavigate()
     const [loginObj, setLoginObj] = useState({})
     const clickHandler = () => {
-
-        axios.post('/login' , (loginObj)).then((data) => {
+        console.log(loginObj + " is login")
+        axios.post('/api/login' , loginObj).then((data) => {
             if (data.status === 200){
                 nav('/home')
             }
-            else {
-                alert('Incorrect pw / username, or back end not yet configured')
+            else if (data.status === 500){
+                alert('Incorrect pw / username')
             }
+        }).catch((error) => {
+            console.log(error)
+            if (error.response.status === 500 && error.response.data.err === "User not found"){
+                alert('Username not found. Check username, or create a new account')
+            }
+            else if (error.response.status === 500 && error.response.data.err === "incorrect password"){
+                alert('password incorrect')
+            }
+            else if (error.response.status === 500) alert("test worked")
+            else {
+                alert('Username or password incorrect')
+            }   
         })
     }
 
     const createAccount = () => {
-
        nav('/create')
     }
 
@@ -54,5 +66,5 @@ const Login = () => {
     </div>
     )
 }
-
 export default Login
+
