fb862ae7163eb3f44d41c246153276a65d939d77
Oliver <103220077+OliverTheScholar@users.noreply.github.com>
2023-08-07 12:25:07 -0400

    modified buddy controller to properly accept contract data and notify buddy

--- server/controllers/buddyController.js
+++ server/controllers/buddyController.js
@@ -1,23 +1,27 @@
 // const Contract = require('../models/contractModel.js');
 // const process = require('process');
 
+const User = require('../models/userModel.js');
+
 require('dotenv').config();
 
 const buddyController = {};
 
 buddyController.notifyBuddy = async (req, res, next) => {
     try {
-        const username = 'Oliver'
-        const buddyName = 'James';
-        const buddyNumber = '+16463428783'
-        const goal = 'lose 20 pounds';
+
+        const user = await User.findOne({_id: req.cookies.ssid})
+        const username = user.username;
+        const buddyName = req.body.buddies[0].name;
+        const goal = req.body.goal;
+        const number = req.body.buddies[0].phoneNumber;
         
         const messageContents = `Hi ${buddyName}, ${username} is on a mission to ${goal} and has added you as an accountability partner.`
         const accountSid = process.env.TWILIO_ACCOUNT_SID;
         const authToken = process.env.TWILIO_AUTH_TOKEN;
         const client = require('twilio')(accountSid, authToken);
         client.messages
-            .create({from: '+18332749971', body: messageContents, to: '+16463428783'})
+            .create({from: '+18332749971', body: messageContents, to: number})
             .then(message => console.log(message.sid));
         next();
     }
